53e98c5dce9b58e8dfd08255c69c2cd22b7acc19
Agene Sunday <sundayojimagene@gmail.com>
2019-12-10 15:31:37 +0100

    addedImage

---
+++ scripts/scripts.js
@@ -0,0 +1,20 @@
+var over1    = document.getElementById("ashback1");
+var over2    = document.getElementById("ashback2");
+var over3    = document.getElementById("ashback3");
+
+over1.addEventListener("mouseover",function (){
+    //over1.style.backgroundColor ="grey";
+   // over1.style.color = 'purple';
+});
+over2.addEventListener("mouseover",() =>{
+    //over2.style.backgroundColor ="grey";
+    //over2.style.color = 'purple';
+
+});
+over3.addEventListener("mouseover",()=>{
+   // over3.style.backgroundColor ="grey";
+    //over3.style.color = 'purple';
+
+});
+
+   
